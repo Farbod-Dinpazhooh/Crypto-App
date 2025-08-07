cb7d8b50f5f1a34d4151ac59e4a9b0a694070d7f
Farbod-Dinpazhooh <farboddinpazhooh35@gmail.com>
2025-08-07 17:54:57 +0330

    Drawing a diagram

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,4 +1,15 @@
 import { useState } from "react";
+import {
+  CartesianGrid,
+  LineChart,
+  Line,
+  Legend,
+  ResponsiveContainer,
+  YAxis,
+  XAxis,
+  Tooltip,
+} from "recharts";
+
 import { convertData } from "../../helpers/convertData";
 
 import styles from "./Chart.module.css";
@@ -15,9 +26,33 @@ function Chart({ chart, setChart }) {
       >
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
